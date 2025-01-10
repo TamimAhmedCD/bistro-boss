2cfaff495895cca23aedf25aa925436b561722db
Tamim Ahmed <tamim20072@gmail.com>
2025-01-11 01:35:53 +0600

    update menu item request

--- src/pages/Dashboard/UpdateItem/UpdateItem.jsx
+++ src/pages/Dashboard/UpdateItem/UpdateItem.jsx
@@ -1,13 +1,127 @@
 import { useLoaderData } from "react-router-dom";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaUtensils } from "react-icons/fa6";
 
 const UpdateItem = () => {
-    const item = useLoaderData()
-    console.log(item);
+  const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+  const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
+  const {name, category, recipe, price, _id} = useLoaderData();
+  const { register, handleSubmit, reset } = useForm();
+  const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+
+    const imageFile = { image: data.image[0] };
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    });
+    if (res.data.success) {
+      // now send menu item data to the server with image the url
+      const menuItem = {
+        name: data.name,
+        category: data.category,
+        price: parseFloat(data.price),
+        recipe: data.recipe,
+        image: res.data.data.display_url,
+      };
+      const menuRes = await axiosSecure.patch(`/menu/${_id}`, menuItem);
+      console.log(menuRes.data);
+      if (menuRes.data.modifiedCount > 0) {
+        // reset();
+        // show success message
+        Swal.fire({
+          title: "Good job!",
+          text: "Item Updated to the menu",
+          timer: 1500,
+          showConfirmButton: false,
+        });
+      }
+    }
+    console.log("with image url", res.data);
+  };
+
   return (
     <div>
       <SectionTitle heading="Update Item" subHeading="Refresh Info" />
-
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Recipe Name*</span>
+            </div>
+            <input
+              type="text"
+              defaultValue={name}
+              placeholder="Recipe Name"
+              {...register("name", { required: true })}
+              className="input input-bordered w-full"
+            />
+          </label>
+          <div className="flex gap-4 my-6">
+            {/* category */}
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text">Category*</span>
+              </div>
+              <select defaultValue={category}
+                {...register("category", { required: true })}
+                className="select select-bordered w-full"
+              >
+                <option value='default' disabled selected>
+                  Select a Category
+                </option>
+                <option value="salad">Salad</option>
+                <option value="pizza">Pizza</option>
+                <option value="soup">Soup</option>
+                <option value="dessert">Dessert</option>
+                <option value="drinks">Drinks</option>
+              </select>
+            </label>
+            {/* price */}
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text">Price*</span>
+              </div>
+              <input
+                type="number"
+                defaultValue={price}
+                placeholder="Recipe Name"
+                {...register("price", { required: true })}
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          <label className="form-control">
+            <div className="label">
+              <span className="label-text">Recipe Details*</span>
+            </div>
+            <textarea
+            defaultValue={recipe}
+              {...register("recipe", { required: true })}
+              className="textarea textarea-bordered h-24"
+              placeholder="Recipe Details"
+            ></textarea>
+          </label>
+          <div className="my-6">
+            <input
+              {...register("image", { required: true })}
+              type="file"
+              className="file-input w-full max-w-xs"
+            />
+          </div>
+          <button className="btn btn-neutral flex gap-2" type="submit">
+            Update Item <FaUtensils></FaUtensils>
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
