9410831de70a9190dd5f8bbe5baeae094fa6db60
Tamim Ahmed <tamim20072@gmail.com>
2025-01-12 19:02:15 +0600

    setup stripe

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,11 +1,17 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import {loadStripe} from '@stripe/stripe-js';
+import {Elements} from '@stripe/react-stripe-js'
+import CheckOutForm from "./CheckOutForm";
 
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK)
 const Payment = () => {
     return (
         <div>
             <SectionTitle heading="Payment" subHeading="Please pay to eat" />
-            <div>
-                <h2 className="text-4xl">Heading</h2>
+            <div className="w-4/6 mx-auto">
+                <Elements stripe={stripePromise}>
+                    <CheckOutForm></CheckOutForm>
+                </Elements>
             </div>
         </div>
     );
